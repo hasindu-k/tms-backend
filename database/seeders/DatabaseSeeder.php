40765e96293bee78620742458f11be6f7d01e6b8
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-10-22 20:03:54 +0530

    Remove unused seeders from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            TaskSeeder::class,
             RolesAndPermissionsSeeder::class,
-            CommentSeeder::class,
         ]);
     }
 }
