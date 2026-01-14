5b72f68c6e60a059185f212952f2330915ac724a
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2026-01-14 17:03:21 +0530

    Add migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migration', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration run successfully: ' . Artisan::output();
 });
